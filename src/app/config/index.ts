b13e592e66770d46a75b0f7bce02547821c499b3
KhalidMimMuzahid <khalidmimm@gmail.com>
2023-11-22 21:58:17 +0600

    structured server side

---
+++ src/app/config/index.ts
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+// dotenv.config({ path: process.cwd()as string + "/.env" })
+dotenv.config({ path: path.join(process.cwd(), '/.env') });
+
+export default {
+  port: process.env.PORT,
+  database_url: process.env.DATABASE_URL,
+};
